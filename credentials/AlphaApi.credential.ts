d86f3a21e0818248727c4084c3644610e080cd7c
Emad Issa <elbembo@gmail.com>
2025-10-05 01:55:51 +0300

    init

---
+++ credentials/AlphaApi.credential.ts
@@ -0,0 +1,32 @@
+// eslint-disable-next-line n8n-nodes-base/cred-filename-against-convention
+import {
+	IAuthenticateGeneric,
+	ICredentialType,
+	INodeProperties,
+} from 'n8n-workflow';
+
+export class AlphaApi implements ICredentialType {
+	// eslint-disable-next-line n8n-nodes-base/cred-class-field-name-uppercase-first-char
+	name = 'AlphaApi';
+	displayName = 'Alpha API';
+	// Uses the link to this tutorial as an example
+	// Replace with your own docs links when building your own nodes
+	documentationUrl = 'https://docs.n8n.io/integrations/creating-nodes/build/declarative-style-node/';
+	properties: INodeProperties[] = [
+		{
+			displayName: 'API Key',
+			name: 'apiKey',
+			type: 'string',
+			typeOptions: { password: true },
+			default: '',
+		},
+	];
+	authenticate = {
+		type: 'generic',
+		properties: {
+			qs: {
+				'api_key': '={{$credentials.apiKey}}'
+			}
+		},
+	} as IAuthenticateGeneric;
+}
